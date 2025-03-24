2b45d9c2767b629ee6dd7d89001792fd67959521
Danyal Rana <ranadanyalarshad@gmail.com>
2025-03-25 00:01:47 +0500

    Add JWT authentication middleware and export user model

---
+++ Module 2 (Intro to Backend)/server/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import ApiError from "../utils/ApiError";
+import asyncHanlder from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+import User from "../models/user.model.js";
+
+const verifyJWT = asyncHanlder (async (req, res, next) => {
+    try {
+        
+        const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+        if(!token) {
+            throw new ApiError(400, "token hai he nahi");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+        if(!user) {
+            throw new ApiError(400, "ghalat token hai.");
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        throw new ApiError(400, error?.message || "Ghalat token bhaija hai ap nay.");
+    }
+})
