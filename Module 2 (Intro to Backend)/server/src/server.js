c558670e7ab345c2b347f4d77d5a0d6c49c332b5
Danyal Rana <ranadanyalarshad@gmail.com>
2025-03-24 22:13:05 +0500

    Day 1

---
+++ Module 2 (Intro to Backend)/server/src/server.js
@@ -0,0 +1,13 @@
+import app from "app.js";
+import dotenv from "dotenv"
+import connectDB from "./config/db";
+
+dotenv.config();
+
+connectDB().then ( () => {
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port number ${process.env.PORT}`);
+    });
+    
+});
