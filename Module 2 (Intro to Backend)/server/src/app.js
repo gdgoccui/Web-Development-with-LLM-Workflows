6ef182bd626995ce199fadaf3828f352347299ec
Danyal Rana <ranadanyalarshad@gmail.com>
2025-03-25 23:20:04 +0500

    Add user authentication features with token generation and cookie handling

--- Module 2 (Intro to Backend)/server/src/app.js
+++ Module 2 (Intro to Backend)/server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
 }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true})); //nested objects allowed
 
 export default app;
--- Module 2 (Intro to Backend)/server/src/controllers/user.controller.js
+++ Module 2 (Intro to Backend)/server/src/controllers/user.controller.js
@@ -1,11 +1,32 @@
 import User from "../models/user.model";
 import ApiError from "../utils/ApiError";
+import ApiResponse from "../utils/ApiResponse";
 import asyncHanlder from "../utils/asyncHandler";
+
+// functin for generating tokens
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+        
+        const myUser = await User.findById(userId);
+
+        const accessToken = myUser.generateAccessToken();
+        const refreshToken = myUser.generateRefreshToken();
+
+        myUser.refreshToken = refreshToken;
+        await myUser.save({validateBeforeSave: false});
+
+        return {accessToken, refreshToken};
+
+
+    } catch (error) {
+        throw new ApiError(500, "token can't be generated");
+    }
+}
 
 // signup
 const registerUser = asyncHanlder( async (req, res, next) => {
     // user ka data
-    const {username, email, address, password, dob, country, city}= req.body;
+    const {username, email, address, password, dob, country, city} = req.body;
 
     // data thek hai ya khrab hai
     if ([username, email, address, password, dob, country, city].some((i) => i?.trim()==="")) {
@@ -32,13 +53,134 @@ const registerUser = asyncHanlder( async (req, res, next) => {
     if(!user) {
         throw new ApiError(500, "User save ni ho ska.");
     }
-} )
+
+    return res.status(200).json(
+        new ApiResponse (200, user, "User registered.")
+    )
+});
+
 // login
+const loginUser = asyncHanlder( async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if (!(email || password)) {
+        throw new ApiError(400, "Email or password is missing");
+    }
+
+    const user = await User.findOne({email});
+
+    if (!user || ! (await bcrypt.compare(password, user.password))) {
+        throw new ApiError(400, "user not find or wrong password");
+    }
+
+    const {accessToken, refreshToken} = generateAccessTokenAndRefreshToken(user._id);
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: false
+    }
+
+    return res
+    .status(200)
+    .cookie("accesstoken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, {
+            user: loggedInUser, accessToken, refreshToken
+        }, "User has been loggedin.")
+    )
+});
+
 
 // logout
+const logoutUser = asyncHanlder(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user._id, {
+        $set: {
+            refreshToken: undefined
+        }
+    }, {new: true});
+
+    const options = {
+        httpOnly: true,
+        secure: false
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, {}, "user logged out successfully.")
+    )
+});
 
 // accountDeletion
+const deleteUser = asyncHanlder ( async (req, res, next) => {
+    await User.findByIdAndDelete(req.user._id);
 
-// forgetPassword
+    res.clearCookie("accessToken");
 
-// updateName
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {}, "user account deleted successfully")
+    )
+});
+
+// update Password
+const updatePassword = asyncHanlder (async (req, res, next) => {
+    const {oldPassword, newPassword} = req.body;
+
+    if (!oldPassword || !newPassword) {
+        throw new ApiError(400, "old password or new password is missing");
+    }
+
+    const user = await User.findById(req.user._id).select("+password");
+
+    const arePasswordsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+    if (!arePasswordsCorrect) {
+        throw new ApiError(400, "old password is incorrect");
+    }
+
+    user.password = await bcrypt.hash(newPassword, 10);
+    await user.save();
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "password has been updated"))
+});
+
+// updateUsername
+const updateUsernam = asyncHanlder ( async (req, res, next) => {
+    const {username} = req.body;
+
+    if (!username) {
+        throw new ApiError (400, "username is required")
+    }
+
+    req.user.username = username;
+    await req.user.save();
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, req.user, "Username has been updated.")
+    )
+});
+
+
+
+// updateFullName
+// updateCountry
+// updateCity
+// updatePhoneNo.
+// updateDOB
+// updateAddress
+//updateEmail
+
+// to do in free time
+// forgetPassword (node mailer)
+// profilePic (multer, aws/cloudinary)
+
